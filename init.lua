7fd531d909610d903a0e38d8ac622adbb38d2961
Ray <raysilverstech@gmail.com>
2023-09-29 20:30:37 -0500

    changed the step over and step into key bindings

--- init.lua
+++ init.lua
@@ -281,8 +281,8 @@ vim.keymap.set('i', 'jj', '<Esc>', {});
 
 -- Mapping for Debugger
 vim.keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>");
-vim.keymap.set('n', '<F10>', ":lua require'dap'.step_over()<CR>");
-vim.keymap.set('n', '<F11>', ":lua require'dap'.step_into()<CR>");
+vim.keymap.set('n', '<F3>', ":lua require'dap'.step_over()<CR>");
+vim.keymap.set('n', '<F2>', ":lua require'dap'.step_into()<CR>");
 vim.keymap.set('n', '<F12>', ":lua require'dap'.step_out()<CR>");
 vim.keymap.set('n', '<leader>b', ":lua require'dap'.toggle_breakpoint()<CR>");
 vim.keymap.set('n', '<leader>B', ":lua require'dap'.set_breakpoint()<CR>");
